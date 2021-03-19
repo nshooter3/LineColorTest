48f9f718f15bbbacff5685fb4bb029e92ebfb0bd
nshooter <nick.shooter@gmail.com>
2021-03-19 16:12:35 -0400

    Added follower

--- Assets/Scripts/BezierTest.cs
+++ Assets/Scripts/BezierTest.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using PathCreation;
 
 public class BezierTest : MonoBehaviour
 {
-    //public BezierShape 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
