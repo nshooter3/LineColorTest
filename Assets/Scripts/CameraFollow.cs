40aa009b81c335b839664ba2ea5e2f7c6fcffbcf
nshooter <nick.shooter@gmail.com>
2021-03-19 17:27:30 -0400

    Camera now follows player, cleaned up level

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Follower player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Follower>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -6,12 +6,13 @@ public class Follower : MonoBehaviour
     public PathCreator pathCreator;
     public float speed = 5f;
     float distanceTravelled;
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
         distanceTravelled += speed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled) + offset;
         transform.eulerAngles = pathCreator.path.GetRotationAtDistance(distanceTravelled).eulerAngles;
     }
 }
