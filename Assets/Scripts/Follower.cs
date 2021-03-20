12b5c21818c3f4c7c2c2bb8a10f0881e2ca55a7f
nshooter <nick.shooter@gmail.com>
2021-03-19 22:31:26 -0400

    tweaked player physics and removed shadows

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -4,16 +4,33 @@ using PathCreation;
 public class Follower : MonoBehaviour
 {
     public PathCreator pathCreator;
-    public float speed = 5f;
+    public float maxSpeed;
+    private float currentSpeed;
+    public float acceleration;
+    public float deceleration;
     float distanceTravelled;
     public Vector3 offset;
+
+    private void Start()
+    {
+        transform.position = pathCreator.path.GetPointAtDistance(0f) + offset;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (isMoving())
         {
-            distanceTravelled += speed * Time.deltaTime;
+            currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration);
+        }
+
+        if (currentSpeed > 0f)
+        {
+            distanceTravelled += currentSpeed * Time.deltaTime;
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled) + offset;
             transform.eulerAngles = pathCreator.path.GetRotationAtDistance(distanceTravelled).eulerAngles;
         }
