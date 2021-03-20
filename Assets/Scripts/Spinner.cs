47c133837b9543eb84b302759e6a7ab9efa0aaf9
nshooter <nick.shooter@gmail.com>
2021-03-19 22:55:39 -0400

    added spinners

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + rotationSpeed * Time.deltaTime, transform.eulerAngles.z);
+    }
+}
