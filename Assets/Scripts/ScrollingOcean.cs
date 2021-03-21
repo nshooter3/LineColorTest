48fba73e49cac66fa6cd5649c0b4742637ea851c
nshooter <nick.shooter@gmail.com>
2021-03-21 14:05:25 -0400

    Added scrolling ocean shader

---
+++ Assets/Scripts/ScrollingOcean.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ScrollingOcean : MonoBehaviour
+{
+    public Renderer ren;
+    public float xSpeed, ySpeed;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        ren.material.SetFloat("_ScrollXSpeed", xSpeed);
+        ren.material.SetFloat("_ScrollYSpeed", ySpeed);
+        ren.material.SetColor("_Color", color);
+    }
+}
