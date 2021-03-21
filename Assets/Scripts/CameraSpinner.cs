989075cd7c26b506ce99a8b94b61d6f840b9ddc2
nshooter <nick.shooter@gmail.com>
2021-03-21 19:23:02 -0400

    Camera now rotates upon victory

---
+++ Assets/Scripts/CameraSpinner.cs
@@ -0,0 +1,17 @@
+﻿public class CameraSpinner : Spinner
+{
+    private bool isSpinning = false;
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        if (isSpinning)
+        {
+            base.Update();
+        }
+        else if(GameStateManager.instance.gameState == GameStateManager.GameState.Win)
+        {
+            isSpinning = true;
+        }
+    }
+}
--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
     public float rotationSpeed;
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + rotationSpeed * Time.deltaTime, transform.eulerAngles.z);
     }
